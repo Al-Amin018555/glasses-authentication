f9697677049ffbad25185fd48b3aaa0f467a7ec7
Al-Amin018555 <alamin15-3799@diu.edu.bd>
2026-01-16 22:37:53 +0600

    observer added todetect user

--- src/FirebaseProvider/FirebaseProvider.jsx
+++ src/FirebaseProvider/FirebaseProvider.jsx
@@ -1,16 +1,25 @@
-import { createUserWithEmailAndPassword } from "firebase/auth";
-import { createContext } from "react";
+import { createUserWithEmailAndPassword, onAuthStateChanged } from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
 import auth from "../firebase/firebase.init";
 
 export const AuthContext = createContext(null);
 
 const FirebaseProvider = ({ children }) => {
-
+    const [user, setUser] = useState(null);
     //create user
     const createUser = (email, password) => {
-       return createUserWithEmailAndPassword(auth, email, password);
+        return createUserWithEmailAndPassword(auth, email, password);
     }
- 
+
+    useEffect(() => {
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+                setUser(user);
+            }
+
+        });
+    }, []);
+
     const allValues = {
         createUser,
     };
